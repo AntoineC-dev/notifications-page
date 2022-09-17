c79bfb3a036bc2b4dcef759bf09b7911747fec2f
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-17 04:08:06 -0400

    Mark as read if click on any link on a notification

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,9 +1,11 @@
 import { Component, Show } from 'solid-js';
 import type { AppNotification } from '../../types';
+import { setNotificationReadById } from '../../stores/notifications';
 
 import styles from './Notification.module.css';
 
 const Notification: Component<AppNotification> = (props: AppNotification) => {
+  const onNavigate = () => !props.read && setNotificationReadById(props.id, true);
   return (
     <article classList={{ [styles.container]: true, [styles.read]: props.read }}>
       <div class={styles.avatar} aria-hidden="true">
@@ -11,25 +13,25 @@ const Notification: Component<AppNotification> = (props: AppNotification) => {
       </div>
       <div class={styles.content}>
         <p class={styles.top}>
-          <a href={props.profile.href} class={styles.name}>
+          <a href={props.profile.href} onClick={onNavigate} class={styles.name}>
             {props.profile.name}
           </a>
           <span>{props.text}</span>
           <Show when={props.link}>
-            <a href={props.link?.href} data-type={props.link?.type} class={styles.link}>
+            <a href={props.link?.href} onClick={onNavigate} data-type={props.link?.type} class={styles.link}>
               {props.link?.label}
             </a>
           </Show>
         </p>
         <span class={styles.createdAt}>{props.createdAt}</span>
         <Show when={props.private_message}>
-          <a href={props.private_message?.href} class={styles.privateMessage}>
+          <a href={props.private_message?.href} onClick={onNavigate} class={styles.privateMessage}>
             {props.private_message?.content}
           </a>
         </Show>
       </div>
       <Show when={props.image}>
-        <a href={props.image?.href} class={styles.image}>
+        <a href={props.image?.href} onClick={onNavigate} class={styles.image}>
           <span class="sr-only">{props.image?.alt}</span>
           <img src={props.image?.src} alt={props.image?.alt} />
         </a>
--- src/stores/notifications.ts
+++ src/stores/notifications.ts
@@ -4,6 +4,9 @@ import { NOTIFICATIONS } from '../data';
 
 const [store, setStore] = createStore<NotificationStore>(NOTIFICATIONS);
 
+export const setNotificationReadById = (id: string, value: boolean) =>
+  setStore((notification) => notification.id === id, 'read', value);
+
 export const setAllNotificationsAsRead = () => setStore((notification) => !notification.read, 'read', true);
 
 export default store;
