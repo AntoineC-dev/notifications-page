4f26874cf1507b9100f150bed6d27ba03b2e6575
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-17 03:18:52 -0400

    Revert "Correct typos"
    This reverts commit d2783addbec81df7fda260111ec995e05d9e5f87.

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -5,34 +5,26 @@ import styles from './Notification.module.css';
 
 const Notification: Component<AppNotification> = (props: AppNotification) => {
   return (
-    <article classList={{ [styles.container]: true, [styles.read]: props.read }}>
+    <article class={styles.container}>
       <div class={styles.avatar} aria-hidden="true">
         <img src={props.avatar} alt="" />
       </div>
       <div class={styles.content}>
-        <p class={styles.top}>
-          <a href={props.profile.href} class={styles.name}>
-            {props.profile.name}
+        <span class={styles.name}>{props.name}</span>
+        <span class={styles.text}>{props.text}</span>
+        <Show when={props.link}>
+          <a href="#" data-type={props.link?.type} class={styles.link}>
+            {props.link?.label}
           </a>
-          <span>{props.text}</span>
-          <Show when={props.link}>
-            <a href={props.link?.href} data-type={props.link?.type} class={styles.link}>
-              {props.link?.label}
-            </a>
-          </Show>
-        </p>
-        <span class={styles.createdAt}>{props.createdAt}</span>
-        <Show when={props.private_message}>
-          <a href={props.private_message?.href} class={styles.privateMessage}>
-            {props.private_message?.content}
-          </a>
+        </Show>
+        <Show when={!props.read}>
+          <span class={styles.dot} aria-hidden="true"></span>
         </Show>
       </div>
       <Show when={props.image}>
-        <a href={props.image?.href} class={styles.image}>
-          <span class="sr-only">{props.image?.alt}</span>
-          <img src={props.image?.src} alt={props.image?.alt} />
-        </a>
+        <div class={styles.image} aria-hidden="true">
+          <img src={props.image} alt="" />
+        </div>
       </Show>
     </article>
   );
