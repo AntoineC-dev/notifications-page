60a2b1b6624f7fd8e4679b4d1512c0cc32b2575c
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-17 03:46:12 -0400

    Link header to the store

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,15 @@
-import { Component } from 'solid-js';
+import { Component, createMemo } from 'solid-js';
+import notifications, { setAllNotificationsAsRead } from '../../stores/notifications';
 
 import styles from './Header.module.css';
 
 const Header: Component = () => {
-  const onClick = () => alert('Marked as read');
+  const unreadCount = createMemo(() => notifications.filter((n) => !n.read).length);
   return (
     <header class={styles.container}>
       <h1 class={styles.title}>Notifications</h1>
-      <span class={styles.count}>3</span>
-      <button onClick={onClick} class={styles.btn}>
+      <span class={styles.count}>{unreadCount()}</span>
+      <button onClick={setAllNotificationsAsRead} class={styles.btn}>
         Mark all as read
       </button>
     </header>
