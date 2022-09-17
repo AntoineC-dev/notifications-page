d2783addbec81df7fda260111ec995e05d9e5f87
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-17 03:18:00 -0400

    Correct typos

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -5,26 +5,34 @@ import styles from './Notification.module.css';
 
 const Notification: Component<AppNotification> = (props: AppNotification) => {
   return (
-    <article class={styles.container}>
+    <article classList={{ [styles.container]: true, [styles.read]: props.read }}>
       <div class={styles.avatar} aria-hidden="true">
         <img src={props.avatar} alt="" />
       </div>
       <div class={styles.content}>
-        <span class={styles.name}>{props.name}</span>
-        <span class={styles.text}>{props.text}</span>
-        <Show when={props.link}>
-          <a href="#" data-type={props.link?.type} class={styles.link}>
-            {props.link?.label}
+        <p class={styles.top}>
+          <a href={props.profile.href} class={styles.name}>
+            {props.profile.name}
           </a>
-        </Show>
-        <Show when={!props.read}>
-          <span class={styles.dot} aria-hidden="true"></span>
+          <span>{props.text}</span>
+          <Show when={props.link}>
+            <a href={props.link?.href} data-type={props.link?.type} class={styles.link}>
+              {props.link?.label}
+            </a>
+          </Show>
+        </p>
+        <span class={styles.createdAt}>{props.createdAt}</span>
+        <Show when={props.private_message}>
+          <a href={props.private_message?.href} class={styles.privateMessage}>
+            {props.private_message?.content}
+          </a>
         </Show>
       </div>
       <Show when={props.image}>
-        <div class={styles.image} aria-hidden="true">
-          <img src={props.image} alt="" />
-        </div>
+        <a href={props.image?.href} class={styles.image}>
+          <span class="sr-only">{props.image?.alt}</span>
+          <img src={props.image?.src} alt={props.image?.alt} />
+        </a>
       </Show>
     </article>
   );
